3b079044e9abe3f79af85f5fba0f23aa012efd75
baowk <baowk1024@gmail.com>
2023-09-07 12:40:36 +0800

    rsa

--- common/utils/cryptos/rsa.go
+++ common/utils/cryptos/rsa.go
@@ -11,34 +11,41 @@ import (
 	"fmt"
 )
 
+// // 将RSA私钥转换为字符串
+// func PrivateKeyToPemStr(privateKey *rsa.PrivateKey) string {
+// 	return string(PrivateKeyToPem(privateKey))
+// }
+
 // 将RSA私钥转换为字符串
-func PrivateKeyToPem(privateKey *rsa.PrivateKey) (string, error) {
+func PrivateKeyToPem(privateKey *rsa.PrivateKey) []byte {
 	privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
 	privateKeyBlock := &pem.Block{
 		Type:  "RSA PRIVATE KEY",
 		Bytes: privateKeyBytes,
 	}
-	privateKeyStr := string(pem.EncodeToMemory(privateKeyBlock))
-	return privateKeyStr, nil
+	return pem.EncodeToMemory(privateKeyBlock)
 }
 
 // 将RSA公钥转换为字符串
-func PublicKeyToPem(publicKey *rsa.PublicKey) (string, error) {
-	publicKeyBytes, err := x509.MarshalPKIXPublicKey(publicKey)
+func PublicKeyToPemStr(publicKey *rsa.PublicKey) (string, error) {
+	b, err := PublicKeyToPem(publicKey)
 	if err != nil {
 		return "", err
+	}
+	return string(b), nil
+}
+
+// 将RSA公钥转换为字符串
+func PublicKeyToPem(publicKey *rsa.PublicKey) ([]byte, error) {
+	publicKeyBytes, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		return nil, err
 	}
 	publicKeyBlock := &pem.Block{
 		Type:  "PUBLIC KEY",
 		Bytes: publicKeyBytes,
 	}
-	publicKeyStr := string(pem.EncodeToMemory(publicKeyBlock))
-	return publicKeyStr, nil
-}
-
-// 将字符串转换为RSA私钥
-func ParsePrivateKey(privateKeyStr string) (*rsa.PrivateKey, error) {
-	return ParsePriKey([]byte(privateKeyStr))
+	return pem.EncodeToMemory(publicKeyBlock), nil
 }
 
 func ParsePriKey(privateKey []byte) (*rsa.PrivateKey, error) {
@@ -51,10 +58,6 @@ func ParsePriKey(privateKey []byte) (*rsa.PrivateKey, error) {
 		return nil, err
 	}
 	return priKey, nil
-}
-
-func ParsePublicKey(publicKeyStr string) (*rsa.PublicKey, error) {
-	return ParsePubKey([]byte(publicKeyStr))
 }
 
 // 将字符串转换为RSA公钥
@@ -75,30 +78,43 @@ func ParsePubKey(publicKey []byte) (*rsa.PublicKey, error) {
 }
 
 // 生成密钥对
-func GenerateRsaKey(len int) (string, string, error) {
+func GenerateRsaKeyStr(len int) (publicKey string, privateKey string, err error) {
+	pub, pri, err := GenerateRsaKey(len)
+	if err != nil {
+		return
+	}
+	publicKey = string(pub)
+	privateKey = string(pri)
+	return
+}
+
+// 生成密钥对
+func GenerateRsaKey(len int) (publicKey []byte, privateKey []byte, err error) {
 	if len != 1024 && len != 4096 {
 		len = 2048
 	}
 	// 生成RSA密钥对
 	key, err := rsa.GenerateKey(rand.Reader, len)
 	if err != nil {
-		return "", "", err
+		return
 	}
 
-	publicKeyStr, err := PublicKeyToPem(&key.PublicKey)
+	publicKey, err = PublicKeyToPem(&key.PublicKey)
 	if err != nil {
-		return "", "", err
+		return
 	}
-	privateKeyStr, err := PrivateKeyToPem(key)
-	if err != nil {
-		return "", "", err
-	}
-	return publicKeyStr, privateKeyStr, nil
+	privateKey = PrivateKeyToPem(key)
+	return
 }
 
 // 公钥加密
 func RSA_Encrypt(message []byte, pubKey string) (string, error) {
-	publicKey, err := ParsePublicKey(pubKey)
+	return RsaEncrypt(message, []byte(pubKey))
+}
+
+// 公钥加密
+func RsaEncrypt(message []byte, pubKey []byte) (string, error) {
+	publicKey, err := ParsePubKey(pubKey)
 	if err != nil {
 		return "", err
 	}
@@ -111,8 +127,8 @@ func RSA_Encrypt(message []byte, pubKey string) (string, error) {
 }
 
 // 私钥解密
-func RSA_Decrypt(encryptedMsg, priKey string) ([]byte, error) {
-	privateKey, err := ParsePrivateKey(priKey)
+func RsaDecrypt(encryptedMsg string, priKey []byte) ([]byte, error) {
+	privateKey, err := ParsePriKey(priKey)
 	if err != nil {
 		return nil, err
 	}
@@ -124,16 +140,31 @@ func RSA_Decrypt(encryptedMsg, priKey string) ([]byte, error) {
 	return rsa.DecryptPKCS1v15(rand.Reader, privateKey, cipherText)
 }
 
+// 私钥解密
+func RSA_Decrypt(encryptedMsg, priKey string) ([]byte, error) {
+	return RsaDecrypt(encryptedMsg, []byte(priKey))
+}
+
 // RsaSign 私钥加签
-func RsaSign(publicKeyPEM string, message []byte) ([]byte, error) {
-	privateKey, _ := ParsePrivateKey(publicKeyPEM)
+func RSA_Sign(publicKeyPEM string, message []byte) ([]byte, error) {
+	return RsaSign([]byte(publicKeyPEM), message)
+}
+
+// RsaSign 私钥加签
+func RsaSign(publicKeyPEM, message []byte) ([]byte, error) {
+	privateKey, _ := ParsePriKey(publicKeyPEM)
 	hash := sha256.Sum256(message)
 	return rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hash[:])
 }
 
 // RsaVerify 公钥验签
-func RsaVerify(publicKeyPEM string, message []byte, signature []byte) error {
-	publicKey, _ := ParsePublicKey(publicKeyPEM)
+func RSA_Verify(publicKeyPEM string, message []byte, signature []byte) error {
+	return RsaVerify([]byte(publicKeyPEM), message, signature)
+}
+
+// RsaVerify 公钥验签
+func RsaVerify(publicKeyPEM, message []byte, signature []byte) error {
+	publicKey, _ := ParsePubKey(publicKeyPEM)
 	hash := sha256.Sum256(message)
 	return rsa.VerifyPKCS1v15(publicKey, crypto.SHA256, hash[:], signature)
 }
