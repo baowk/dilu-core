a54dd07bff3b18c34bfcb8ebc3d780acb383c75f
baowk <baowk1024@gmail.com>
2023-09-07 11:55:45 +0800

    rsa

--- common/utils/cryptos/rsa.go
+++ common/utils/cryptos/rsa.go
@@ -1,8 +1,10 @@
 package cryptos
 
 import (
+	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/x509"
 	"encoding/hex"
 	"encoding/pem"
@@ -36,87 +38,102 @@ func PublicKeyToString(publicKey *rsa.PublicKey) (string, error) {
 
 // 将字符串转换为RSA私钥
 func ParsePrivateKey(privateKeyStr string) (*rsa.PrivateKey, error) {
-	privateKeyBlock, _ := pem.Decode([]byte(privateKeyStr))
+	return ParsePriKey([]byte(privateKeyStr))
+}
+
+func ParsePriKey(privateKey []byte) (*rsa.PrivateKey, error) {
+	privateKeyBlock, _ := pem.Decode(privateKey)
 	if privateKeyBlock == nil || privateKeyBlock.Type != "RSA PRIVATE KEY" {
 		return nil, fmt.Errorf("无效的私钥")
 	}
-	privateKey, err := x509.ParsePKCS1PrivateKey(privateKeyBlock.Bytes)
+	priKey, err := x509.ParsePKCS1PrivateKey(privateKeyBlock.Bytes)
 	if err != nil {
 		return nil, err
 	}
-	return privateKey, nil
+	return priKey, nil
+}
+
+func ParsePublicKey(publicKeyStr string) (*rsa.PublicKey, error) {
+	return ParsePubKey([]byte(publicKeyStr))
 }
 
 // 将字符串转换为RSA公钥
-func ParsePublicKey(publicKeyStr string) (*rsa.PublicKey, error) {
-	publicKeyBlock, _ := pem.Decode([]byte(publicKeyStr))
+func ParsePubKey(publicKey []byte) (*rsa.PublicKey, error) {
+	publicKeyBlock, _ := pem.Decode(publicKey)
 	if publicKeyBlock == nil || publicKeyBlock.Type != "PUBLIC KEY" {
 		return nil, fmt.Errorf("无效的公钥")
 	}
-	publicKey, err := x509.ParsePKIXPublicKey(publicKeyBlock.Bytes)
+	pubKey, err := x509.ParsePKIXPublicKey(publicKeyBlock.Bytes)
 	if err != nil {
 		return nil, err
 	}
-	rsaPublicKey, ok := publicKey.(*rsa.PublicKey)
+	rsaPublicKey, ok := pubKey.(*rsa.PublicKey)
 	if !ok {
 		return nil, fmt.Errorf("无效的RSA公钥")
 	}
 	return rsaPublicKey, nil
 }
 
-func GenerateRsaKey(len int) (string, string) {
-	if len == 0 {
+// 生成密钥对
+func GenerateRsaKey(len int) (string, string, error) {
+	if len != 1024 && len != 4096 {
 		len = 2048
 	}
 	// 生成RSA密钥对
 	key, err := rsa.GenerateKey(rand.Reader, len)
 	if err != nil {
-		fmt.Println("无法生成RSA密钥对：", err)
-		panic(err)
+		return "", "", err
 	}
 
 	publicKeyStr, err := PublicKeyToString(&key.PublicKey)
 	if err != nil {
-		panic(err)
+		return "", "", err
 	}
 	privateKeyStr, err := PrivateKeyToString(key)
 	if err != nil {
-		panic(err)
+		return "", "", err
 	}
-	return publicKeyStr, privateKeyStr
-
+	return publicKeyStr, privateKeyStr, nil
 }
 
-func RSA_Encrypt(message, pubKey string) string {
-
+// 公钥加密
+func RSA_Encrypt(message []byte, pubKey string) (string, error) {
 	publicKey, err := ParsePublicKey(pubKey)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 	// 使用公钥加密消息
-	encryptedMessage, err := rsa.EncryptPKCS1v15(rand.Reader, publicKey, []byte(message))
+	encryptedMessage, err := rsa.EncryptPKCS1v15(rand.Reader, publicKey, message)
 	if err != nil {
 		panic(err)
 	}
-
-	return hex.EncodeToString(encryptedMessage)
+	return hex.EncodeToString(encryptedMessage), nil
 }
 
-func RSA_Decrypt(encryptedMsg, priKey string) string {
+// 私钥解密
+func RSA_Decrypt(encryptedMsg, priKey string) ([]byte, error) {
 	privateKey, err := ParsePrivateKey(priKey)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	// 使用私钥解密消息
 	cipherText, err := hex.DecodeString(encryptedMsg)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	decryptedMessage, err := rsa.DecryptPKCS1v15(rand.Reader, privateKey, cipherText)
-	if err != nil {
-		panic(err)
-	}
+	return rsa.DecryptPKCS1v15(rand.Reader, privateKey, cipherText)
+}
 
-	return string(decryptedMessage[:])
+// RsaSign 私钥加签
+func RsaSign(publicKeyPEM string, message []byte) ([]byte, error) {
+	privateKey, _ := ParsePrivateKey(publicKeyPEM)
+	hash := sha256.Sum256(message)
+	return rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hash[:])
+}
 
+// RsaVerify 公钥验签
+func RsaVerify(publicKeyPEM string, message []byte, signature []byte) error {
+	publicKey, _ := ParsePublicKey(publicKeyPEM)
+	hash := sha256.Sum256(message)
+	return rsa.VerifyPKCS1v15(publicKey, crypto.SHA256, hash[:], signature)
 }
