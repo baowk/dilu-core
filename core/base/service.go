5e116742acd211962b132a15e03611ed36062e75
baowk <baowk1024@gmail.com>
2023-09-09 17:20:49 +0800

    db

--- core/base/service.go
+++ core/base/service.go
@@ -3,6 +3,7 @@ package base
 import (
 	"github.com/baowk/dilu-core/core"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 type BaseService struct {
@@ -10,43 +11,47 @@ type BaseService struct {
 	C      *gin.Context
 }
 
+func (s *BaseService) DB() *gorm.DB {
+	return core.Db(s.DbName)
+}
+
 func (s *BaseService) MakeContext(c *gin.Context) {
 	s.C = c
 }
 
 func (s *BaseService) Create(data any) error {
-	return core.Db(s.DbName).Create(data).Error
+	return s.DB().Create(data).Error
 }
 
 func (s *BaseService) Save(data any) error {
-	return core.Db(s.DbName).Save(data).Error
+	return s.DB().Save(data).Error
 }
 
 func (s *BaseService) Get(id any, model any) error {
-	return core.Db(s.DbName).First(model, id).Error
+	return s.DB().First(model, id).Error
 }
 
 func (s *BaseService) DelWhere(where any) error {
-	return core.Db(s.DbName).Delete(where).Error
+	return s.DB().Delete(where).Error
 }
 
 func (s *BaseService) DelIds(model any, ids any) error {
-	return core.Db(s.DbName).Delete(model, ids).Error
+	return s.DB().Delete(model, ids).Error
 }
 
 func (s *BaseService) Page(where any, data any, total *int64, limit, offset int) error {
-	return core.Db(s.DbName).Where(where).Limit(limit).Offset(offset).
+	return s.DB().Where(where).Limit(limit).Offset(offset).
 		Find(data).Limit(-1).Offset(-1).Count(total).Error
 }
 
 func (s *BaseService) UpdateWhere(where any, updates map[string]any) error {
-	return core.Db(s.DbName).Where(where).Updates(updates).Error
+	return s.DB().Where(where).Updates(updates).Error
 }
 
 func (s *BaseService) UpdateWhereModel(where any, updates any) error {
-	return core.Db(s.DbName).Where(where).Updates(updates).Error
+	return s.DB().Where(where).Updates(updates).Error
 }
 
 func (s *BaseService) GetByWhere(where any, model any) error {
-	return core.Db(s.DbName).Where(where).Find(model).Error
+	return s.DB().Where(where).Find(model).Error
 }
