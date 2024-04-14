618533590feb2e28b633f55dbc8fe07cae016188
baowk <baowk1024@gmail.com>
2024-04-14 11:55:12 +0800

    解决redis集群下分布式锁问题

--- core/cache/cache.go
+++ core/cache/cache.go
@@ -25,46 +25,29 @@ type ICache interface {
 func New(conf config.CacheCfg) ICache {
 	if conf.GetType() == "redis" {
 		arr := strings.Split(conf.Addr, ";")
-		if len(arr) > 1 {
-			clusterClient := redis.NewClusterClient(&redis.ClusterOptions{
-				Addrs:        arr,
-				Password:     conf.Password,   // 设置密码
-				DialTimeout:  5 * time.Second, // 设置连接超时
-				ReadTimeout:  5 * time.Second, // 设置读取超时
-				WriteTimeout: 5 * time.Second, // 设置写入超时
-			})
-			pong, err := clusterClient.Ping(context.Background()).Result()
-			if err != nil {
-				panic("redis connect ping failed, err:" + err.Error())
-			} else {
-				fmt.Println("redis connect ping response:", "pong", pong)
-				r := RedisCache{
-					mode:          2,
-					clusterClient: clusterClient,
-					prefix:        conf.Prefix,
-				}
-				return &r
-			}
-		} else {
-			rdb := redis.NewClient(&redis.Options{
-				Addr:     conf.Addr,
-				Password: conf.Password, // no password set
-				DB:       conf.DB,       // use default DB
-			})
-			pong, err := rdb.Ping(context.Background()).Result()
-			if err != nil {
-				panic("redis connect ping failed, err:" + err.Error())
-			} else {
-				fmt.Println("redis connect ping response:", "pong", pong)
-				r := RedisCache{
-					mode:   1,
-					redis:  rdb,
-					prefix: conf.Prefix,
-				}
-				return &r
-			}
+		op := &redis.UniversalOptions{
+			Addrs:    arr,
+			Password: conf.Password, // no password set
 		}
+		if conf.DB > 0 {
+			op.DB = conf.DB
+		}
+		if conf.MasterName != "" {
+			op.MasterName = conf.MasterName
+		}
+		rdb := redis.NewUniversalClient(op)
 
+		pong, err := rdb.Ping(context.Background()).Result()
+		if err != nil {
+			panic("redis connect ping failed, err:" + err.Error())
+		} else {
+			fmt.Println("redis connect ping response:", "pong", pong)
+			r := RedisCache{
+				redis:  rdb,
+				prefix: conf.Prefix,
+			}
+			return &r
+		}
 	} else {
 		return NewMemory()
 	}
--- core/cache/redis.go
+++ core/cache/redis.go
@@ -8,10 +8,11 @@ import (
 )
 
 type RedisCache struct {
-	mode          int8 //1 单机 2 cluster
-	redis         *redis.Client
-	clusterClient *redis.ClusterClient
-	prefix        string
+	redis  redis.UniversalClient
+	prefix string
+	//mode  int8 //1 单机 2 cluster
+	//clusterClient *redis.ClusterClient
+
 }
 
 func (c *RedisCache) Type() string {
@@ -22,100 +23,58 @@ func (c *RedisCache) Get(key string) (string, error) {
 	if c.prefix != "" {
 		key = c.prefix + ":" + key
 	}
-	if c.mode == 1 {
-		return c.redis.Get(context.TODO(), key).Result()
-	} else {
-		return c.clusterClient.Get(context.TODO(), key).Result()
-	}
+	return c.redis.Get(context.TODO(), key).Result()
 }
 
 func (c *RedisCache) Set(key string, val any, expiration time.Duration) error {
 	if c.prefix != "" {
 		key = c.prefix + ":" + key
 	}
-	if c.mode == 1 {
-		return c.redis.Set(context.TODO(), key, val, expiration).Err()
-	} else {
-		return c.clusterClient.Set(context.TODO(), key, val, expiration).Err()
-	}
+	return c.redis.Set(context.TODO(), key, val, expiration).Err()
 }
 
 func (c *RedisCache) Del(key string) error {
 	if c.prefix != "" {
 		key = c.prefix + ":" + key
 	}
-
-	if c.mode == 1 {
-		return c.redis.Del(context.TODO(), key).Err()
-	} else {
-		return c.clusterClient.Del(context.TODO(), key).Err()
-	}
+	return c.redis.Del(context.TODO(), key).Err()
 }
 
 func (c *RedisCache) HGet(hk, field string) (string, error) {
 	if c.prefix != "" {
 		hk = c.prefix + ":" + hk
 	}
-
-	if c.mode == 1 {
-		return c.redis.HGet(context.TODO(), hk, field).Result()
-	} else {
-		return c.clusterClient.HGet(context.TODO(), hk, field).Result()
-	}
+	return c.redis.HGet(context.TODO(), hk, field).Result()
 }
 
 func (c *RedisCache) HDel(hk, fields string) error {
 	if c.prefix != "" {
 		hk = c.prefix + ":" + hk
 	}
-
-	if c.mode == 1 {
-		return c.redis.HDel(context.TODO(), hk, fields).Err()
-	} else {
-		return c.clusterClient.HDel(context.TODO(), hk, fields).Err()
-	}
+	return c.redis.HDel(context.TODO(), hk, fields).Err()
 }
 
 func (c *RedisCache) Incr(key string) error {
 	if c.prefix != "" {
 		key = c.prefix + ":" + key
 	}
-
-	if c.mode == 1 {
-		return c.redis.Incr(context.TODO(), key).Err()
-	} else {
-		return c.clusterClient.Incr(context.TODO(), key).Err()
-	}
+	return c.redis.Incr(context.TODO(), key).Err()
 }
 
 func (c *RedisCache) Decr(key string) error {
 	if c.prefix != "" {
 		key = c.prefix + ":" + key
 	}
-
-	if c.mode == 1 {
-		return c.redis.Decr(context.TODO(), key).Err()
-	} else {
-		return c.clusterClient.Decr(context.TODO(), key).Err()
-	}
+	return c.redis.Decr(context.TODO(), key).Err()
 }
 
 func (c *RedisCache) Expire(key string, expiration time.Duration) error {
 	if c.prefix != "" {
 		key = c.prefix + ":" + key
 	}
-
-	if c.mode == 1 {
-		return c.redis.Expire(context.TODO(), key, expiration).Err()
-	} else {
-		return c.clusterClient.Expire(context.TODO(), key, expiration).Err()
-	}
+	return c.redis.Expire(context.TODO(), key, expiration).Err()
 }
 
-func (c *RedisCache) GetClient() *redis.Client {
+func (c *RedisCache) GetClient() redis.UniversalClient {
 	return c.redis
-}
-
-func (c *RedisCache) GetClusterClient() *redis.ClusterClient {
-	return c.clusterClient
 }
