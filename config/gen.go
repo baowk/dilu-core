90eddf8d1d3d05fba56894722508077f76c45b21
baowk <baowk1024@gmail.com>
2026-03-08 18:22:44 +0800

    生成增加配置

--- config/gen.go
+++ config/gen.go
@@ -1,6 +1,9 @@
 package config
 
 type GenCfg struct {
-	Enable    bool   `mapstructure:"enable" json:"enable" yaml:"ebable"`             // 开启生成
-	FrontPath string `mapstructure:"front-path" json:"front-path" yaml:"front-path"` // 前端路径
+	Enable       bool              `mapstructure:"enable" json:"enable" yaml:"enable"`                      // 开启生成
+	GenFront     bool              `mapstructure:"gen-front" json:"gen-front" yaml:"gen-front"`             // 是否生成前端代码
+	FrontPath    string            `mapstructure:"front-path" json:"front-path" yaml:"front-path"`          // 前端路径
+	TemplatePath string            `mapstructure:"template-path" json:"template-path" yaml:"template-path"` // 模板根目录
+	ModuleMap    map[string]string `mapstructure:"module-map" json:"module-map" yaml:"module-map"`          // dbName -> moduleName
 }
